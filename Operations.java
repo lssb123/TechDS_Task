2dab073844d5816987fcb75dd189eea4619da6c0
lssb123 <blikhitsrisai@gmail.com>
2023-08-30 14:30:43 +0530

    Subtraction

--- Operations.java
+++ Operations.java
@@ -3,8 +3,12 @@ public class Operations {
     public static void Addition(int a,int b){
         System.out.println(a+b);
     }
+    public static void Subtraction(int a,int b){
+        System.out.println(a-b);
+    }
     public static void main(String []args){
         Addition(3,4);
+        Subtraction(4,3);
     }
 }
 
