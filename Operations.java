e07593fee74454d9952e2e5eda76ca3d9df9121b
lssb123 <blikhitsrisai@gmail.com>
2023-08-30 14:27:16 +0530

    Addition

---
+++ Operations.java
@@ -0,0 +1,10 @@
+
+public class Operations {
+    public static void Addition(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void main(String []args){
+        Addition(3,4);
+    }
+}
+
