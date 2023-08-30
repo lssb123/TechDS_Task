576f34a1bfebf550c9d89c9bf6502dc5c780a3e0
lssb123 <blikhitsrisai@gmail.com>
2023-08-30 14:34:24 +0530

    Multiplication

--- Operations.java
+++ Operations.java
@@ -6,9 +6,13 @@ public class Operations {
     public static void Subtraction(int a,int b){
         System.out.println(a-b);
     }
+    public static void Multiplication(int a,int b){
+        System.out.println(a*b);
+    }
     public static void main(String []args){
         Addition(3,4);
         Subtraction(4,3);
+        Multiplication(4,3);
     }
 }
 
