b8eb208ad0ccfbb81d36879c196a1ba877773a76
lssb123 <blikhitsrisai@gmail.com>
2023-08-30 14:36:53 +0530

    modulus

--- Operations.java
+++ Operations.java
@@ -9,10 +9,14 @@ public class Operations {
     public static void Multiplication(int a,int b){
         System.out.println(a*b);
     }
+    public static void Modulus(int a,int b){
+        System.out.println(a%b);
+    }
     public static void main(String []args){
         Addition(3,4);
         Subtraction(4,3);
         Multiplication(4,3);
+        Modulus(12,7);
     }
 }
 
